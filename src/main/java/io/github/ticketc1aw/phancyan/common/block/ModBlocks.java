e801a73bf4b227d6fbaaf8ba63c03a3abb875017
ticketc1aw <161030661+ticketc1aw@users.noreply.github.com>
2026-08-26 14:49:52 +0900

    lunacy flower fix

--- src/main/java/io/github/ticketc1aw/phancyan/common/block/ModBlocks.java
+++ src/main/java/io/github/ticketc1aw/phancyan/common/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package io.github.ticketc1aw.phancyan.common.block;
 
 import io.github.ticketc1aw.phancyan.Phancyan;
+import io.github.ticketc1aw.phancyan.common.block.custom.LunacyFlowerBlock;
 import io.github.ticketc1aw.phancyan.common.item.ModItems;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,8 +18,8 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Phancyan.MOD_ID);
 
-    public static final RegistryObject<PhancyanBlock> LUNACY_FLOWER = registerPhancyanBlock("lunacy_flower",
-            () -> new PhancyanBlock(BlockBehaviour.Properties.of(),2));
+    public static final RegistryObject<LunacyFlowerBlock> LUNACY_FLOWER = registerPhancyanBlock("lunacy_flower",
+            () -> new LunacyFlowerBlock(BlockBehaviour.Properties.copy(Blocks.POPPY)));
 
 
 
